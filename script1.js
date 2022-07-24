34f07db0110f1136d84758fe26c2f09e4cb7b1d6
Emmanuel Lyimo <esal29t@gmail.com>
2022-07-24 02:42:29 -0700

    updated the script.js

--- script1.js
+++ script1.js
@@ -6,22 +6,63 @@ let user = {
     age: 36,
     email: 'john@example.com',
     location:'renton',
-    siblings: ['Amani','Inno','Furaha','Neema'],
+    siblings: [{ndugu:'Amani Inno Furaha Neema', average:30},
+               {ndugu:'John Smith JUmara  Inno Vel', average:5}    
+              ],
+
     login: function(){
         console.log('user logged in');
     },
+
     logout: function(){
         console.log('user logged out');
     },
+    logSiblings: function(){
+        // console.log(this.siblings);
+        console.log('my siblings are');
+        this.siblings.forEach(relative =>{
+            console.log(relative.ndugu,relative.average);
+        })
+
+    
+    
+    }
 }
 
-user.age = 40
+// user.age = 40
 console.log(user);
 console.log(user.age)
-user['name'] = 'Doe';
-console.log(user.name);
-console.log(user['location']);
+// user['name'] = 'Doe';
+// console.log(user.name);
+// console.log(user['location']);
 
 
 user.login();
 user.logout();
+user.logSiblings();
+
+
+
+
+// Math Object
+const random  = Math.random();
+
+console.log(random);
+console.log(Math.round(Math.random() * 50));
+
+
+// primitive values
+
+let scoreOne = 50
+let scoreTwo = scoreOne;
+
+console.log(`scoreOne:${scoreOne}`, `scoreTwo:${scoreTwo}`);
+
+scoreOne = 100;
+console.log(`scoreOne:${scoreOne}`, `scoreTwo:${scoreTwo}`);
+
+// reference values
+
+const userOne = {name:'Ally', age:40, location:'Renton'};
+const userTwo = userOne;
+console.log(userOne, userTwo);
