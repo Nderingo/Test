5c3296e29aaf977429caf0dd8fe02035e8b3ab35
Emmanuel Lyimo <esal29t@gmail.com>
2022-07-26 01:36:18 -0700

    about the DOM query Selectors

--- css/new1/sandbos.js
+++ css/new1/sandbos.js
@@ -1,3 +1,12 @@
-const para = document.querySelector('.error');
 
-console.log(para)
+// querySelector and querySelectorAll
+const para = document.querySelectorAll('p');
+para.forEach(para=>{
+    console.log(para);
+});
+// console.log(para)
+
+
+const tags = document.querySelectorAll('div');
+
+console.log(tags[1]);
