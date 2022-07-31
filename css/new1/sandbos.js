d9427cd2276cd26da95ff6d9c408dc6e77306e23
Emmanuel Lyimo <esal29t@gmail.com>
2022-07-31 00:50:50 -0700

    changed the script.js

--- css/new1/sandbos.js
+++ css/new1/sandbos.js
@@ -7,17 +7,24 @@ para.forEach(para=>{
 // console.log(para)
 
 
-const tags = document.querySelectorAll('div');
+// const tags = document.querySelectorAll('div');
 
-console.log(tags[1]);
+// console.log(tags[1]);
 
-const error = document.querySelectorAll('.error');
+// const error = document.querySelectorAll('.error');
 
 // console.log(error)
 
-const hereby = document.getElementsByClassName('.error');
+const hereby = document.querySelectorAll('div');
 hereby.forEach(hereby=>{
     console.log(hereby.innerText);
+    hereby.innerText += 'Hello'
 });
 
 // console.log(hereby.innerText);
+
+// changing the CONTENT_MODE
+
+const content = document.querySelector('.errors');
+
+content.innerHTML = '<h2>THIS IS A NEW HTML</h2>'
